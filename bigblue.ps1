358bd5a8aa47a404d53e248be91ff5b195b50e13
Michael Madden <mikemaddem@gmail.com>
2020-05-07 13:32:17 -0400

    add bigblue.ps1, start messing with spawning ps windows for completing defender scans, etc

---
+++ bigblue.ps1
@@ -0,0 +1,21 @@
+﻿# First Arg
+$param1 = $args[0]
+# Second arg
+$param2 = $args[1]
+
+If($param1 -eq 'lockdown'){
+    write-host 'starting lockdown mode'
+    
+    invoke-expression 'cmd /c start powershell -NoExit -Command {                           `
+    cd -path ~/Desktop;                  `
+    $host.UI.RawUI.WindowTitle = "BigBlue Defender Scan";                                `
+    color -background "red";
+    clear;
+    write-host "hello";                                                          `
+}';
+}
+
+else{
+write-host "Syntax Error"
+write-host "Proper Syntax: bigblue.ps1 lockdown"
+}
