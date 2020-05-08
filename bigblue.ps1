03f3c47c7a7f29e20b0fb344058afbb386bb45b4
Michael Madden <mikemaddem@gmail.com>
2020-05-08 16:23:02 -0400

    Add basic block ip command for inbound and outbound tcp/udp traffic

--- bigblue.ps1
+++ bigblue.ps1
@@ -13,8 +13,23 @@ If($param1 -eq 'lockdown'){
     clear;
     write-host "hello";                                                          `
 }';
+    write-host "Verify that Windows Defender Service is running"
+    Get-Service "WinDefend"
 }
-
+If($param1 -eq 'blockip' -And $param2 -eq 'out'){
+    write-host "Blocking ip for outbound traffic"
+    $ip = Read-Host "Enter the ip address like x.x.x.x"
+    echo "Blocking outbound tcp and udp for $ip"
+    New-NetFirewallRule -DisplayName "Block $ip" -Direction Outbound -LocalPort Any -Protocol TCP -Action Block -RemoteAddress $ip
+    New-NetFirewallRule -DisplayName "Block $ip" -Direction Outbound -LocalPort Any -Protocol UDP -Action Block -RemoteAddress $ip
+}
+If($param1 -eq 'blockip' -And $param2 -eq 'in'){
+    write-host "Blocking ip for outbound traffic"
+    $ip = Read-Host "Enter the ip address like x.x.x.x"
+    echo "Blocking outbound tcp and udp for $ip"
+    New-NetFirewallRule -DisplayName "Block $ip" -Direction Inbound -LocalPort Any -Protocol TCP -Action Block -RemoteAddress $ip
+    New-NetFirewallRule -DisplayName "Block $ip" -Direction Inbound -LocalPort Any -Protocol UDP -Action Block -RemoteAddress $ip
+}
 else{
 write-host "Syntax Error"
 write-host "Proper Syntax: bigblue.ps1 lockdown"
