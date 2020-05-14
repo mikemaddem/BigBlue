e34fe198bc799f26189578124198ff4af4a3386c
Michael Madden <mikemaddem@gmail.com>
2020-05-14 15:47:03 -0400

    added gporeport and gpo hunt commands

--- bigblue.ps1
+++ bigblue.ps1
@@ -47,9 +47,66 @@ elseif($param1 -eq 'blockip' -And $param2 -eq 'in'){
     New-NetFirewallRule -DisplayName "Block $ip" -Direction Inbound -LocalPort Any -Protocol TCP -Action Block -RemoteAddress $ip
     New-NetFirewallRule -DisplayName "Block $ip" -Direction Inbound -LocalPort Any -Protocol UDP -Action Block -RemoteAddress $ip
 }
+elseif($param1 -eq 'gporeport'){
+    Import-Module GroupPolicy
+    Get-GPOReport -All -ReportType HTML | out-file .\BigBlue-GPOReport.html
+}
+elseif($param1 -eq 'gporeport' -And $param2 -eq 'hunt'){
+    Import-Module grouppolicy
+$ErrorActionPreference ='silentlycontinue'
+
+# Key strings for scheduled tasks and logon/logoff scripts
+$schedtasks = "scheduledtask"
+$on_off = "command>"
+
+$DomainName = $env:USERDNSDOMAIN
+$GPOs = Get-GPO -All -Domain $DomainName
+write-host "Finding all the GPOs in $DomainName"
+
+# Search through each GPO's XML for the specific strings
+Write-Host "Starting search...."
+foreach ($gpo in $GPOs) 
+    {
+    $report = Get-GPOReport -Guid $gpo.Id -ReportType Xml
+    if (($report -match $schedtasks) -or ($report -match $on_off)) 
+        {
+        write-host "********** Match(es) found in: $($gpo.DisplayName) **********"
+        $report1 = $report -split ' '| sls 'name=', 'starthour=', 'args=','startIn=', 'comment=', 'filters'| Get-Unique
+        $report2 = $report -split ' '| sls 'command>'
+
+        if ($report1 -ne $null)
+            {
+            "#######################"
+            "#   SCHEDULEDTASKS    #"   
+            "#######################"   
+
+             $report_out = ($report1 -replace '"',"")
+             for ($i=6;$i -lt $report_out.count;$i+=7) 
+                {
+                $report_out[$i] = ' '
+                }
+             $report_out
+            "#######################"
+            " "
+            }
+        if ($report2 -ne $null)
+            {
+            $r2 = [string]$report2
+            "#############################"
+            "#   LOGON / LOGOFF SCRIPTS  #"   
+            "#############################"
+            ($r2.Split('<')).split('>')[2,6,10,14,18,22,26,30,34]
+            "#############################"
+            " "
+            }
+        }
+    }
+}
 elseif($param1 -eq 'commands' -Or $param1 -eq 'help'){
     write-host "Listing all commands"
     write-host "blockip <in/out> ---- block an ip address for inbound or outbound tcp and udp traffic."
+    write-host 'gporeport --- outputs a gpo report in a html file'
+    write-host 'gporeport hunt ---- search through all gpos for scheduled tasks and logon/logoff scripts'
     write-host "lockdown"
 }
 else{
